bcb72744ea87cb370f91d974c741617dd856d375
Marcos ribeiro <marcosribeiro@dotcompany.com.br>
2024-12-25 20:08:15 -0300

    ATUALIZAÇÃO SUGERIDA PELO GOOGLE AI.

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -3,26 +3,57 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TranslateModule } from '@ngx-translate/core';
+import { CadastroAlunoModule } from '../cadastro-aluno/cadastro-aluno.module'; // Importe o seu módulo aqui
 
 import FindLanguageFromKeyPipe from './language/find-language-from-key.pipe';
 import TranslateDirective from './language/translate.directive';
-import { AlertComponent } from './alert/alert.component';
-import { AlertErrorComponent } from './alert/alert-error.component';
+import AlertComponent from './alert/alert.component';
+import AlertErrorComponent from './alert/alert-error.component';
+import HasAnyAuthorityDirective from './auth/has-any-authority.directive';
+import DurationPipe from './date/duration.pipe';
+import FormatMediumDatetimePipe from './date/format-medium-datetime.pipe';
+import FormatMediumDatePipe from './date/format-medium-date.pipe';
+import SortByDirective from './sort/sort-by.directive';
+import SortDirective from './sort/sort.directive';
+import ItemCountComponent from './pagination/item-count.component';
 
-/**
- * Application wide Module
- */
 @NgModule({
-  imports: [AlertComponent, AlertErrorComponent, FindLanguageFromKeyPipe, TranslateDirective],
+  imports: [
+    CommonModule,
+    NgbModule,
+    FontAwesomeModule,
+    TranslateModule,
+    CadastroAlunoModule, // Adicione o módulo na lista de imports
+  ],
+  declarations: [
+    FindLanguageFromKeyPipe,
+    TranslateDirective,
+    AlertComponent,
+    AlertErrorComponent,
+    HasAnyAuthorityDirective,
+    DurationPipe,
+    FormatMediumDatetimePipe,
+    FormatMediumDatePipe,
+    SortByDirective,
+    SortDirective,
+    ItemCountComponent,
+  ],
   exports: [
     CommonModule,
     NgbModule,
     FontAwesomeModule,
-    AlertComponent,
-    AlertErrorComponent,
     TranslateModule,
     FindLanguageFromKeyPipe,
     TranslateDirective,
+    AlertComponent,
+    AlertErrorComponent,
+    HasAnyAuthorityDirective,
+    DurationPipe,
+    FormatMediumDatetimePipe,
+    FormatMediumDatePipe,
+    SortByDirective,
+    SortDirective,
+    ItemCountComponent,
   ],
 })
-export default class SharedModule {}
+export class SharedModule {}
